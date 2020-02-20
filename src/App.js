1d3ed7c6631c86c879ce4a87ae22305c1e2a4dce
Charlie DiFranco <charlie.difranco2@gmail.com>
2020-02-19 18:03:29 -0600

    still working through dates dropdown

--- src/App.js
+++ src/App.js
@@ -1,13 +1,50 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import "./App.css";
+import axios from "axios";
+
 
 function App() {
+  const [data, setData] = useState([])
+  const [dates, setDates] = useState([])
+
+  useEffect(() => {
+    axios.get('https://api.nasa.gov/planetary/apod?api_key=DEMO_KEY')
+      .then(res => {
+        setData(res.data)
+        console.log(res.data)
+      })
+      .catch(err => { console.log("Could not get")
+      })
+    },[]);
+
+
+  let today = new Date();
+  
+  useEffect(() => {
+    const generateDates = (startDate) => {
+      const datesArr = [];
+      let prevDay;
+      while (today.valueOf() > startDate.valueOf()){
+        prevDay = new Date(today.setDate(today.getDate() - 1))
+        datesArr.push(prevDay)
+        today = prevDay;
+      }
+      setDates(datesArr);
+      console.log(datesArr)
+    };
+    
+    generateDates(new Date(2020, 0, 1))
+  },[])
+
   return (
     <div className="App">
-      <p>
-        Read through the instructions in the README.md file to build your NASA
-        app! Have fun 🚀!
-      </p>
+        <img alt="image of the day" src={data.url}></img>
+        <p>Date: {data.date}</p>
+        <p>Description: {data.description}</p>
+        <div>
+          <h1>Select a date:</h1>
+          <div></div>
+        </div>
     </div>
   );
 }
